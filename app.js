6189d7afe6bc811ee60a407a0b36867104d994f5
johnathan robinson <johnathan.l.robinson@gmail.com>
2025-02-24 21:02:31 -0600

    changes to loops exercise

--- app.js
+++ app.js
@@ -1,7 +1,75 @@
 console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+//Exercise 1 
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+for ( let i =0; i < 100; i++){
+    if (i % 2 == 0) {
+        continue;
+    } else {
+        console.log (i);
+    }
+}
+
+ //Exercise 2 
+
+
+
+for  (let i = 1; i <= 100; i++) {
+    if ( i % 3 === 0 && i % 5 === 0 ) {
+        console.log ("FIZZBUZZ");
+    } else if ( i % 3 === 0) {
+        console.log("FIZZ");
+    } else if (i % 5 === 0) {
+        console.log("BUZZ");
+     } else {
+        console.log(i);
+     }
+    }
+
+
+    //Exercise 3
+
+
+    let i = 1 ;
+    while ( i <= 100 ) {
+if (i % 2 !== 0 ) {
+    console.log (i);
+}
+i ++;
+}
+
+
+let j = 1;
+do {
+if (j% 2 !== 0) {
+    console.log (j); 
+}
+j++; 
+} while (j <= 100);
+
+
+let c = 1; 
+while (c <=100) {
+    if (c % 3 === 0 && c % 5 ===0) {
+        console.log("FIZZBUZZ");
+    } else if ( c % 5 === 0)  {
+        console.log (c);
+    }
+    c++;
+}
+
+
+//Exercise 4
+
+let found = false;
+for ( i = 0; i <= n; i ++ )  {
+    if ( i === value ) {
+        console.log ("Found value!");
+        found = true;
+        break; 
+    }
+}
+
+if ( !found) {
+    console.log("Did not find value");
+}
